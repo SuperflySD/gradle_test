f4adb391c0bfb8cb4c6cb37dd1b95ff1c5306cf5
sergey <sergei.dorofeev@clarivate.com>
2020-11-14 11:52:36 +0300

    A

--- src/main/java/suka/MainClass.java
+++ src/main/java/suka/MainClass.java
@@ -1,30 +1,30 @@
 package suka;
 
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@SpringBootApplication
+@RestController
+@SpringBootApplication
 public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("Smth");
-//        Logger logger = LoggerFactory.getLogger(MainClass.class);
-//        logger.info("Hello World");
-//        SpringApplication.run(MainClass.class, args);
+        Logger logger = LoggerFactory.getLogger(MainClass.class);
+        logger.info("Hello World");
+        SpringApplication.run(MainClass.class, args);
     }
 
 
-//    @RequestMapping(value = {"/", "/main", "/result", "/compare"}, method = RequestMethod.GET)
-//    public String comtr() {
-//        return "bootApplication \n";
-//    }
+    @RequestMapping(value = {"/", "/main", "/result", "/compare"}, method = RequestMethod.GET)
+    public String comtr() {
+        return "bootApplication \n";
+    }
 
 }
